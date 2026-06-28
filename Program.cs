b61f4d06f73ec14e0e9f993b0e895a2d59d921a2
Neil Hoang <neil.hoang0721@gmail.com>
2026-06-27 19:19:04 -0700

    Run database migrations on startup

--- Program.cs
+++ Program.cs
@@ -24,6 +24,11 @@ builder.Services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.Authentic
 
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
